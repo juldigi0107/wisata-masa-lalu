31326bbd0eff2ac635ac82e07c8b18e128b597b6
juldigi0107 <tvvvnp4rtn@privaterelay.appleid.com>
2026-09-15 15:59:59 +0700

    style: keep archive recovery CSS on critical path

--- src/main.jsx
+++ src/main.jsx
@@ -22,6 +22,7 @@ import "./world/entry-flow-v2.css";
 import "./world/page-system.css";
 import "./world/feature-deep-dive-v4.css";
 import "./world/seasonal-function-v4.css";
+import "./world/archive-recovery-v4.css";
 
 Object.assign(baseCatalog, assembledCatalog);
 
