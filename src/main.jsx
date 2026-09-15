cbe764f1875229a1924107a94d4b03665698c4fe
juldigi0107 <tvvvnp4rtn@privaterelay.appleid.com>
2026-09-15 16:04:05 +0700

    style: load differentiated v4 mechanic presentation

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import "./world/motion.css";
 import "./world/polish.css";
 import "./world/special-mechanics.css";
 import "./world/mechanics-v2.css";
+import "./world/mechanics-diversity-v4.css";
 import "./world/device-controls.css";
 import "./world/resilience.css";
 import "./world/mobile-premium.css";
